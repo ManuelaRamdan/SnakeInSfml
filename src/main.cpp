77d031ebc6ff3fef866857ef910fa1bc3e4b25d5
Manuela <mramdan@gmail.com>
2024-10-30 21:22:50 -0300

    actualizo el juego

--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,130 @@
 #include "sfml-utn-inspt.h"
-
+// constantes de la ventana
 const unsigned int ANCHO_VENT = 800;
 const unsigned int ALTO_VENT = 600;
 const unsigned int FRAMERATE = 60;
 
+//-------------------------------------------------------------------------
+// creamos la variable direccion t cuanros cuadraditos se mueven
+enum Direccion
+{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+};
+const float MOVIMIENTO = 5.0f;
+
+const float VELOCIDAD = 4.5f;
+const float LADO_CUADRADO = 25.f;
+
 int main()
 {
+    // creamos la ventana
     RenderWindow ventana = RenderWindow({ANCHO_VENT, ALTO_VENT}, "Unidad 1 - Taller de Videojuegos");
     ventana.setFramerateLimit(FRAMERATE);
+    //-------------------------------------------------------------------------
+    // creamos la "manzana"
     CircleShape forma = CircleShape(10, 20);
     forma.setPosition((ANCHO_VENT / 2) + 20, (ALTO_VENT / 2));
+
+    //-------------------------------------------------------------------------
+    // cabeceras de las funciones
     bool colisionConBordesForma(const CircleShape &forma, const RectangleShape &bordeIzquierdo, const RectangleShape &bordeDerecho, const RectangleShape &bordeInferior, const RectangleShape &bordeSuperior);
 
     bool colisionConBordesSnake(const vector<RectangleShape> &snake, const RectangleShape &bordeIzquierdo, const RectangleShape &bordeDerecho, const RectangleShape &bordeInferior, const RectangleShape &bordeSuperior);
 
     bool colisionConBordesRectangulo(const RectangleShape &bloque, const RectangleShape &bordeIzquierdo, const RectangleShape &bordeDerecho, const RectangleShape &bordeInferior, const RectangleShape &bordeSuperior);
 
-    RectangleShape bloque(Vector2f(50, 20));
+    void moverSnake(Direccion & direccion, vector<RectangleShape> & snake);
+
+    bool colisionConBloque(const RectangleShape &bloque, const RectangleShape &cabeza);
+
+    //-------------------------------------------------------------------------
+    // creamos el bloque
+    RectangleShape bloque(Vector2f(80, 20));
     bloque.setPosition(100, 100);
     bloque.setFillColor(Color::Magenta);
 
-    /*Vector2f posMouse = {0, 0};
-    int LADO = 5;
-
-    RectangleShape movil = RectangleShape({LADO, LADO});
-    movil.setFillColor(Color ::Green);
-    movil.setOrigin(LADO / 2.f, LADO / 2.f);*/
-
+    //-------------------------------------------------------------------------
+    // creamos  el "cuerpo" de la serpiente
     vector<RectangleShape> snake;
-    int x = (ANCHO_VENT / 2) - 20;
-    for (int i = 0; i <= 3; i++)
+    int x = (ANCHO_VENT / 2);
+    for (int i = 0; i < 3; i++)
     {
         RectangleShape square(Vector2f(20, 20));
         square.setPosition(x, 180);
-        x = x - 20;
+        x = x - 5;
         snake.push_back(square);
     }
+
+    //-------------------------------------------------------------------------
+    // creamos los bordes
     RectangleShape bordeIzquierdo(Vector2f(20, ALTO_VENT));
     bordeIzquierdo.setFillColor(Color::Red);
     bordeIzquierdo.setPosition(0, 0);
 
     RectangleShape bordeDerecho(Vector2f(20, ALTO_VENT));
-    bordeDerecho.setFillColor(Color::Blue);
+    bordeDerecho.setFillColor(Color::Green);
     bordeDerecho.setPosition(ANCHO_VENT - 20, 0);
 
     RectangleShape bordeInferior(Vector2f(ANCHO_VENT, 20));
     bordeInferior.setFillColor(Color::Green);
     bordeInferior.setPosition(0, ALTO_VENT - 20);
 
-    // Crear borde superior
     RectangleShape bordeSuperior(Vector2f(ANCHO_VENT, 20)); // Cambia el tamaño del borde superior
-    bordeSuperior.setFillColor(Color::Yellow);              // Asegúrate de usar bordeSuperior aquí
+    bordeSuperior.setFillColor(Color::Red);                 // Asegúrate de usar bordeSuperior aquí
     bordeSuperior.setPosition(0, 0);                        // Posición en la parte superior
 
+    //-------------------------------------------------------------------------
+    // mostrar los puntos
     // Agregar un mensaje de puntos
-    /*Font fuente;
-    fuente.loadFromFile("arial.ttf");
+    Font fuente;
+    fuente.loadFromFile("../recursos/TT Octosquares Trial Regular.ttf");
     Text textoPuntos;
     textoPuntos.setFont(fuente);
-    textoPuntos.setCharacterSize(100);
+    textoPuntos.setCharacterSize(20);
     textoPuntos.setFillColor(Color::White);
-    textoPuntos.setPosition(10, 10);
-    textoPuntos.setString("Puntos: 0");*/
+    textoPuntos.setPosition(10, 0);
+    textoPuntos.setString("Puntos: 0");
 
+    Text textoInicio;
+    textoInicio.setFont(fuente);
+    textoInicio.setCharacterSize(30);
+    textoInicio.setFillColor(Color::White);
+    textoInicio.setString("Bienvenido a Snake!\nPresiona ESPACIO para jugar.");
+    textoInicio.setPosition((ANCHO_VENT - textoInicio.getLocalBounds().width) / 2, ALTO_VENT / 2 - 50);
+
+    Text textoPerdiste;
+    textoPerdiste.setFont(fuente);
+    textoPerdiste.setCharacterSize(30);
+    textoPerdiste.setFillColor(Color::Red);
+    textoPerdiste.setString("Perdiste!\n");
+    textoInicio.setPosition((ANCHO_VENT - textoPerdiste.getLocalBounds().width) / 2, ALTO_VENT / 2 - 50);
+    //-------------------------------------------------------------------------
+    // declaramos las variables
+    Direccion direccion = RIGHT;
     int puntos = 0;
+    bool seMovio = true, haMovido = false;
+    bool juegoIniciado = false;
+    bool chocoConBorde = false;
+    float xCuadrado = 1.0, yCuadrado = 1;
+    bool derecha = true, abajo = true;
+    bool mostrarCuadrado = false;
+    //-------------------------------------------------------------------------
+    // Se define un cuadrado de LADO_CUADRADO de lado
+    RectangleShape cuadrado = RectangleShape({LADO_CUADRADO, LADO_CUADRADO});
+    // Color RGB (Rojo, Verde, Azul) entre 0 y 255
+    cuadrado.setFillColor(Color(255, 0, 128));
+
+    // Los diferenciales de movimiento (cuanto avanza el cuadrado) en los ejes x,y
+    Vector2 diff = {VELOCIDAD, VELOCIDAD};
+    //-------------------------------------------------------------------------
+    //-------------------------------------------------------------------------
+    // se abre la ventana
     while (ventana.isOpen())
     {
+
         Event evento = Event();
         while (ventana.pollEvent(evento))
         {
@@ -75,86 +134,60 @@ int main()
                 ventana.close();
             }
         }
-        /*posMouse = Mouse ::getPosition(ventana);
-        movil.setPosition(posMouse.x, posMouse.y);
-
-        // Vector2f movil (posMouse.x, posMouse.y);
-
-        if (movil.getGlobalBounds().intersects(forma.getGlobalBounds()))
+        //-------------------------------------------------------------------------
+        // cuando el juego empieza, muestre la pantalla de inicio
+        if (!juegoIniciado)
         {
-            forma.setPosition(rand() % ANCHO_VENT, rand() % ALTO_VENT);
-        }*/
+            ventana.clear();
+            ventana.draw(textoInicio);
+            ventana.display();
 
-        // forma.setPosition(rand() % ANCHO_VENT+1,rand() % ALTO_VENT+1);
-        // Agregar un borde a la pantalla
-        // Crear bordes
-
-        if (Keyboard::isKeyPressed(Keyboard::Right))
-        {
-            Vector2f position = snake[0].getPosition();
-            for (int i = snake.size() - 1; i >= 0; --i)
+            if (Keyboard::isKeyPressed(Keyboard::Space))
             {
-                if (i == 0)
-                {
-                    // Only move the head in the specified direction
-                    snake[i].setPosition(position.x + 5, position.y);
-                }
-                else
-                {
-
-                    snake[i].setPosition(snake[i - 1].getPosition());
-                }
+                juegoIniciado = true;
+                puntos = 0;
+                // Reiniciar variables del juego si es necesario
             }
-        }
-        if (Keyboard::isKeyPressed(Keyboard::Left))
-        {
-            for (int i = snake.size() - 1; i >= 0; --i)
-            {
-                if (i == 0)
-                {
-                    // Only move the head in the specified direction
-                    snake[i].setPosition(snake[i].getPosition().x - 5, snake[i].getPosition().y);
-                }
-                else
-                {
-                    // The rest of the body just follows the ahead bodypart's direction
-                    snake[i].setPosition(snake[i - 1].getPosition());
-                }
-            }
-        }
-        if (Keyboard::isKeyPressed(Keyboard::Up))
-        {
-            for (int i = snake.size() - 1; i >= 0; --i)
-            {
-                if (i == 0)
-                {
-                    // Only move the head in the specified direction
-                    snake[i].setPosition(snake[i].getPosition().x, snake[i].getPosition().y - 5);
-                }
-                else
-                {
-                    // The rest of the body just follows the ahead bodypart's direction
-                    snake[i].setPosition(snake[i - 1].getPosition());
-                }
-            }
-        }
-        if (Keyboard::isKeyPressed(Keyboard::Down))
-        {
-            for (int i = snake.size() - 1; i >= 0; --i)
-            {
-                if (i == 0)
-                {
-                    // Only move the head in the specified direction
-                    snake[i].setPosition(snake[i].getPosition().x, snake[i].getPosition().y + 5);
-                }
-                else
-                {
-                    // The rest of the body just follows the ahead bodypart's direction
-                    snake[i].setPosition(snake[i - 1].getPosition());
-                }
-            }
+            continue;
         }
 
+        //-------------------------------------------------------------------------
+        // movimiento de la serpiente
+        Direccion nuevaDireccion = direccion;
+
+        if (Keyboard::isKeyPressed(Keyboard::Right) && direccion != LEFT)
+        {
+            nuevaDireccion = RIGHT;
+            haMovido = true;
+        }
+        else if (Keyboard::isKeyPressed(Keyboard::Down) && direccion != UP)
+        {
+            nuevaDireccion = DOWN;
+            haMovido = true;
+        }
+        else if (Keyboard::isKeyPressed(Keyboard::Left) && direccion != RIGHT)
+        {
+            nuevaDireccion = LEFT;
+            haMovido = true;
+        }
+        else if (Keyboard::isKeyPressed(Keyboard::Up) && direccion != DOWN)
+        {
+            nuevaDireccion = UP;
+            haMovido = true;
+        }
+
+        if (nuevaDireccion != direccion)
+        {
+            direccion = nuevaDireccion;
+        }
+
+        if (haMovido)
+        {
+            moverSnake(direccion, snake);
+            // haMovido = false; // Resetear la bandera
+        }
+        //-------------------------------------------------------------------------
+        // movimiento aleatorio de la "manzana"
         if (snake[0].getGlobalBounds().intersects(forma.getGlobalBounds()))
         {
             do
@@ -167,34 +200,101 @@ int main()
             square.setPosition(snake[snake.size() - 1].getPosition());
             snake.push_back(square);
             puntos++;
-            // textoPuntos.setString("Puntos: " + to_string(puntos));
+            textoPuntos.setString("Puntos: " + to_string(puntos));
         }
-
+        //-------------------------------------------------------------------------
+        // que pasa si el snake choca con el borde
         if (colisionConBordesSnake(snake, bordeIzquierdo, bordeDerecho, bordeInferior, bordeSuperior))
         {
+            chocoConBorde = true;
+            puntos = 0;
+        }
+
+        if (chocoConBorde)
+        {
+            ventana.clear();
+            ventana.draw(textoPerdiste); // Dibuja el mensaje de pérdida
+            ventana.display();
+            continue; // Salir del bucle para evitar dibujar el juego
+        }
+        //-------------------------------------------------------------------------
+        // si la snake choca con el bloque
+        if (colisionConBloque(bloque, snake[0]))
+        {
+            puntos = 0;
+            textoPuntos.setString("Puntos: " + to_string(puntos));
+
+            for (int i = 2; i < snake.size(); i++)
+            {
+                snake.pop_back();
+            }
+
             int x = (ANCHO_VENT / 2) - 20;
             for (int i = 0; i <= snake.size(); i++)
             {
-
                 snake[i].setPosition(x, 180);
-                x = x - 20;
+                x = x - 5;
             }
         }
-        //bloque.setPosition(rand() % ANCHO_VENT + 10, rand() % ALTO_VENT);
-        
 
-        // Dentro del bucle principal
-        if (puntos % 5 == 0 && puntos > 0)
+        //-------------------------------------------------------------------------
+        // movimiento del bloque
+        if (puntos % 5 == 0 && puntos > 0 && seMovio)
         {
             do
             {
-                bloque.setPosition(rand() % ANCHO_VENT + 10, rand() % ALTO_VENT);
+                bloque.setPosition(rand() % ANCHO_VENT, rand() % ALTO_VENT);
             } while (colisionConBordesRectangulo(bloque, bordeIzquierdo, bordeDerecho, bordeInferior, bordeSuperior));
-            
-            
+            seMovio = false;
         }
 
-        // Comprobar colisión entre la forma y el bloque
+        if (puntos % 5 != 0)
+        {
+            seMovio = true;
+        }
+        //-------------------------------------------------------------------------
+        // que se agregue otro bloque que se mueva
+        if (puntos >= 15 && puntos <= 30)
+        {
+            cuadrado.move(xCuadrado, yCuadrado);
+
+            if (cuadrado.getPosition().y == (ALTO_VENT / 2))
+            {
+                if (abajo)
+                {
+                    xCuadrado *= -1;
+                    derecha = false;
+                }
+                else
+                {
+                    xCuadrado *= 1;
+                    derecha = false;
+                }
+            }
+
+            if (cuadrado.getPosition().x == (ANCHO_VENT / 2))
+            {
+                if (derecha)
+                {
+                    yCuadrado *= 1;
+                    abajo = false;
+                }
+                else
+                {
+                    yCuadrado *= -1;
+                    abajo = true;
+                }
+            }
+
+            mostrarCuadrado =  true;
+        }
+        if (puntos< 15 || puntos>30)
+        {
+            mostrarCuadrado = false;
+        }
+        
+        //-------------------------------------------------------------------------
+        // que la "manzana" no este donde esta el bloque
         if (forma.getGlobalBounds().intersects(bloque.getGlobalBounds()))
         {
             // Cambiar la posición de la forma a una nueva posición aleatoria
@@ -204,19 +304,25 @@ int main()
             } while (colisionConBordesForma(forma, bordeIzquierdo, bordeDerecho, bordeInferior, bordeSuperior));
         }
 
-        cout << puntos << endl;
-
+        //-------------------------------------------------------------------------
+        // dibuja todos los objetos necesarios
         ventana.clear();
         ventana.draw(bordeIzquierdo);
         ventana.draw(bordeDerecho);
         ventana.draw(bordeInferior);
         ventana.draw(bordeSuperior);
         ventana.draw(bloque);
-        // ventana.draw(textoPuntos);
-        for (const auto &square : snake)
-        {
+        ventana.draw(textoPuntos);
 
-            ventana.draw(square);
+        if (mostrarCuadrado)
+        {
+            ventana.draw(cuadrado);
+        }
+        
+
+        for (int i = 0; i < snake.size(); i++)
+        {
+            ventana.draw(snake[i]);
         }
         ventana.draw(forma);
         ventana.display();
@@ -226,8 +332,8 @@ int main()
 bool colisionConBordesForma(const CircleShape &forma, const RectangleShape &bordeIzquierdo, const RectangleShape &bordeDerecho, const RectangleShape &bordeInferior, const RectangleShape &bordeSuperior)
 {
     return forma.getGlobalBounds().intersects(bordeIzquierdo.getGlobalBounds()) ||
-           forma.getGlobalBounds().intersects(bordeDerecho.getGlobalBounds()) ||
            forma.getGlobalBounds().intersects(bordeInferior.getGlobalBounds()) ||
+           forma.getGlobalBounds().intersects(bordeDerecho.getGlobalBounds()) ||
            forma.getGlobalBounds().intersects(bordeSuperior.getGlobalBounds());
 }
 
@@ -245,4 +351,37 @@ bool colisionConBordesRectangulo(const RectangleShape &bloque, const RectangleSh
            bloque.getGlobalBounds().intersects(bordeDerecho.getGlobalBounds()) ||
            bloque.getGlobalBounds().intersects(bordeInferior.getGlobalBounds()) ||
            bloque.getGlobalBounds().intersects(bordeSuperior.getGlobalBounds());
+}
+
+void moverSnake(Direccion &direccion, vector<RectangleShape> &snake)
+{
+
+    Vector2f cabezaPos = snake[0].getPosition();
+
+    switch (direccion)
+    {
+    case UP:
+        cabezaPos.y -= MOVIMIENTO;
+        break;
+    case DOWN:
+        cabezaPos.y += MOVIMIENTO;
+        break;
+    case LEFT:
+        cabezaPos.x -= MOVIMIENTO;
+        break;
+    case RIGHT:
+        cabezaPos.x += MOVIMIENTO;
+        break;
+    }
+
+    for (int i = snake.size() - 1; i > 0; --i)
+    {
+        snake[i].setPosition(snake[i - 1].getPosition());
+    }
+    snake[0].setPosition(cabezaPos);
+}
+
+bool colisionConBloque(const RectangleShape &bloque, const RectangleShape &cabeza)
+{
+    return cabeza.getGlobalBounds().intersects(bloque.getGlobalBounds());
 }
